822bf138f21271d66e89a1d27433958b6fd06eb9
Fatih-esgi <fatih@fat-e.ch>
2021-04-13 16:32:23 +0200

    upd: debug routing red. to home if random url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,8 @@ const routes: Routes = [
   {
     path: 'tabs',
     loadChildren: () => import('./@features/tabs/tabs.module').then( m => m.TabsModule) 
-  }
+  },
+  { path: '**', redirectTo: 'tabs/home', pathMatch: 'full' },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
