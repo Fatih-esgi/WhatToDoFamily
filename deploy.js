0f82b02a96e8ba378089feb9d6c9eddb791a3502
Fatih-esgi <fatih@fat-e.ch>
2021-04-15 19:24:32 +0200

    fix var username

--- deploy.js
+++ deploy.js
@@ -2,9 +2,9 @@ var FtpDeploy = require("ftp-deploy");
 var ftpDeploy = new FtpDeploy();
 
 var config = {
-    user: $secrets.USERKEY,
+    user: secrets.USERKEY,
     // Password optional, prompted if none given
-    password: $secrets.FTPPASS,
+    password: secrets.FTPPASS,
     host: "what-to-do.ch",
     port: 21,
     localRoot: __dirname + "/dist/WhatToDoFamily",
