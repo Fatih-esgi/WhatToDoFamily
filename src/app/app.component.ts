8006dce6d656b77991c19286f7e722d267fe2729
Fatih-esgi <fatih@fat-e.ch>
2021-05-05 19:46:43 +0200

    :upd loading screen on route change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,15 @@ import { Component } from '@angular/core';
 import { CheckdeviceService } from './@services/update&devices/checkdevice.service';
 import { UpdatesService } from './@services/update&devices/updates.service';
 import { Plugins } from '@capacitor/core';
+import {
+  Router,
+  Event as RouterEvent,
+  NavigationStart,
+  NavigationEnd,
+  NavigationCancel,
+  NavigationError
+} from '@angular/router'
+
 
 @Component({
   selector: 'app-root',
@@ -9,18 +18,39 @@ import { Plugins } from '@capacitor/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'WhatToDoFamily';
+  public showOverlay = true;
 
-constructor(
-  private _checkDevice : CheckdeviceService,
-  private _checkUpdates : UpdatesService,
+  constructor(
+    private _checkDevice: CheckdeviceService,
+    private _checkUpdates: UpdatesService,
+    private router: Router
 
-){
- this._checkDevice.checkDevice();
- this._checkUpdates.checkUpdates();
+  ) {
+    this._checkDevice.checkDevice();
+    this._checkUpdates.checkUpdates();
+    router.events.subscribe((event: RouterEvent) => {
+      this.navigationInterceptor(event)
+    })
 
- 
-}
+  }
+  
+  navigationInterceptor(event: RouterEvent): void {
+    if (event instanceof NavigationStart) {
+      this.showOverlay = true;
+    }
+    if (event instanceof NavigationEnd) {
+      this.showOverlay = false;
+    }
 
+    // Set loading state to false in both of the below events to hide the spinner in case a request fails
+    if (event instanceof NavigationCancel) {
+      this.showOverlay = false;
+    }
+    if (event instanceof NavigationError) {
+      this.showOverlay = false;
+    }
+  }
 
 }
